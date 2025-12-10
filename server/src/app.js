65668a619cce1e41ae6a7cbab0074df52c32499e
Dev-ali94 <sheikhaliimran5452@gmail.com>
2025-12-10 21:14:33 +0500

    first commit

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+import { clerkMiddleware } from '@clerk/express'
+import express from 'express'
+import { serve } from "inngest/express";
+import {inngest,functions} from "../src/inngest/index.js"
+import cors from "cors"
+const app = express()
+
+app.use(express.json())
+app.use(clerkMiddleware())
+app.use(cors({
+    origin:"http://localhost:5173",
+    Credential:true
+}))
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+export default app
